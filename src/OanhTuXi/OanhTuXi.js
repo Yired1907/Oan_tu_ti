00bb5899b94a5c849ec49cc2916d5b0125f8f02f
Qui <thanhqui19072000@gmail.com>
2023-04-30 16:54:31 +0700

    commit message`

--- src/OanhTuXi/OanhTuXi.js
+++ src/OanhTuXi/OanhTuXi.js
@@ -1,30 +1,49 @@
-import React, { Component } from 'react'
-import './OanhTuXi.css'
-import Player from './Player'
-import Computer from './Computer'
-import Ketquatrochoi from './Ketquatrochoi'
-import { connect } from 'react-redux'
+import React, { useState } from 'react';
+import './OanhTuXi.css';
+import Player from './Player';
+import Computer from './Computer';
+import Ketquatrochoi from './Ketquatrochoi';
+import { connect } from 'react-redux';
 
-class OanhTuXi extends Component {
-    render() {
-        return (
-            <div className='gameOanhTuXi'>
-                <div className='row text-center mt-5 '>
-                    <div className='col-4'>
-                        <Player />
-                    </div>
-                    <div className='col-4'>
-                        <Ketquatrochoi />
-                        <button onClick={() => { this.props.Playergame() }} className='btn btn-block btn-outline-warning btn-lg mt-4'>Play game</button>
-                    </div>
-                    <div className='col-4'>
-                        <Computer />
-                    </div>
+function OanhTuXi(props) {
+    const [lastClickTime, setLastClickTime] = useState(0);
+    const [buttonDisabled, setButtonDisabled] = useState(false);
+
+    function handleClick() {
+        const currentTime = Date.now();
+        const elapsedTime = currentTime - lastClickTime;
+        if (elapsedTime >= 4 * 1000) { // 4 seconds in milliseconds
+            // Allow button click
+            props.Playergame();
+            setLastClickTime(currentTime);
+            setButtonDisabled(true);
+            setTimeout(() => {
+                setButtonDisabled(false);
+            }, 0.5 * 800);
+        } else {
+            // Disallow button click
+            // alert(`Please wait ${4 - Math.floor(elapsedTime / 1000)} more seconds before clicking again.`);
+        }
+    }
+
+    return (
+        <div className='gameOanhTuXi'>
+            <div className='row text-center mt-5 '>
+                <div className='col-4'>
+                    <Player />
+                </div>
+                <div className='col-4'>
+                    <Ketquatrochoi />
+                    <button onClick={handleClick} className='btn btn-block btn-outline-warning btn-lg mt-4' disabled={buttonDisabled}>Play game</button>
+                </div>
+                <div className='col-4'>
+                    <Computer />
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
+
 const mapDispatchToProp = (dispatch) => {
     return {
         Playergame: () => {
@@ -33,17 +52,18 @@ const mapDispatchToProp = (dispatch) => {
             let randomComuterItem = setInterval(() => {
                 dispatch({
                     type: 'RAMDOM'
-                })
+                });
                 count++;
                 if (count >= 10) {
-                    //DỪng hàm setInterval
-                    clearInterval(randomComuterItem)
+                    //Dừng hàm setInterval
+                    clearInterval(randomComuterItem);
                     dispatch({
                         type: 'KET_QUA',
-                    })
+                    });
                 }
-            }, 100)
+            }, 100);
         }
-    }
-}
-export default connect(null, mapDispatchToProp)(OanhTuXi)
+    };
+};
+
+export default connect(null, mapDispatchToProp)(OanhTuXi);
